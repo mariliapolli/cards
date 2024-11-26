5ba3c3529242062a5b94d934e2719554e73313a1
marilia polli <00001090907795sp@al.educacao.sp.gov.br>
2024-11-26 17:59:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'O que foi o bug do milenio?',
+    'foi um medo coletivo de que os computadores não conseguissem processar a mudança de milênio, no final do século XX'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor Pais?',
+    'Vaticano'
+)
+
+criaCartao(
+    'Marematica',
+    'Metade de 2?',
+    '1'
+)
+
+criaCartao(
+    'Inglês',
+    'Poder em Inglês?',
+    'Power'
+)
